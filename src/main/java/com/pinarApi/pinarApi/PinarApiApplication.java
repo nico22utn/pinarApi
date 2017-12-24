e766792fb8c9739c15c3ecbba021a4ae70aa38df
Nicolas <nico22utn@gmail.com>
2017-12-24 17:38:56 -0300

    Creacion api pinar plaza

---
+++ src/main/java/com/pinarApi/pinarApi/PinarApiApplication.java
@@ -0,0 +1,12 @@
+package com.pinarApi.pinarApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinarApiApplication.class, args);
+	}
+}
