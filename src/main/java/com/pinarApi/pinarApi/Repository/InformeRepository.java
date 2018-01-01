c0d9a6f347a185e61ab93131803011a01f37d27b
Nicolas <nico22utn@gmail.com>
2018-01-01 20:48:29 -0300

    Cambios

---
+++ src/main/java/com/pinarApi/pinarApi/Repository/InformeRepository.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pinarApi.pinarApi.Repository;
+
+import com.pinarApi.pinarApi.entidades.HistorialClinico;
+import com.pinarApi.pinarApi.entidades.Informe;
+import java.io.Serializable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author User
+ */
+@Repository
+public interface InformeRepository extends JpaRepository<Informe, Serializable>{
+    
+    public Informe findByHistorialClinico(HistorialClinico historial);
+}
